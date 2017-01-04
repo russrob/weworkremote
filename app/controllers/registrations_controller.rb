e9c99a2344b639e9a88e8ab7eb9aaf5e73370480
Russell Roberts <russrob23@gmail.com>
2017-01-03 21:59:43 -0800

    added the functionaitly to the user profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
-	protected
-	def update_resource(resource, params)
-		resource.update_without_password(params)
-	end
+
+  protected
+
+def update_resource(resource, params)
+      if params[:password].blank? && params[:password_confirmation].blank?
+      resource.update_without_password(params)
+  end
+  end
 end
