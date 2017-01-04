c4bf786b78990f06d9c42d60294cb3f6e0714507
Russell Roberts <russrob23@gmail.com>
2017-01-03 20:58:35 -0800

    added the user profile section

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def configure_permitted_paramters
+  	devise_parameter_sanitizer.for(:account_update) << :email << :phone_number << :description << :password
+  	
+  end
 end
