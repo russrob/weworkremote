7b06a70fc8df85ab3d62babb9b7d13b359435d76
Russell Roberts <russrob23@gmail.com>
2017-01-07 21:43:20 -0800

    create photo and ajax delete view

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
-class Room < ApplicationRecord
+class Room < ActiveRecord::Base
   belongs_to :user
+  has_many :photos
 
   validates :home_type, presence: true
   validates :room_type, presence: true
@@ -7,7 +8,6 @@ class Room < ApplicationRecord
   validates :bed_room, presence: true
   validates :bath_room, presence: true
   validates :listing_name, presence: true, length: {maximum: 50}
-  validates :summary, presence: true
-  validates :address, presence: true, length: {maximum: 500}
-  
+  validates :summary, presence: true, length: {maximum: 500}
+  validates :address, presence: true
 end
